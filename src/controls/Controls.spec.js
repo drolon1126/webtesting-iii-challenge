bef7364a1b9fb2f38582049a4413064ba4359b1b
danRolon <daniel.rolon@yahoo.com>
2019-08-20 00:17:49 -0500

    added click test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -6,49 +6,59 @@ import Controls from './Controls';
 
 afterEach(cleanup);
 
-const toggleLocked = () => {
-  this.setState(prev => ({ locked: !prev.locked }));
-};
-
-const toggleClosed = () => {
-  this.setState(prev => ({ closed: !prev.closed }));
-};
-
 describe('<Controls />', () => {
 
   it('check if both buttons render', () => {
-    const container = render(<Controls locked={false} closed={false} toggleLocked={toggleLocked} toggleClosed={toggleClosed}/>);
+    const container = render(<Controls locked={false} closed={false}/>);
     expect(document.querySelectorAll('.toggle-btn').length).toBe(2);
    });
 
    it('check text on lock button if unlocked then Lock Gate', () => {
-    const container = render(<Controls locked={false} closed={false} toggleLocked={toggleLocked} toggleClosed={toggleClosed}/>);
+    const container = render(<Controls locked={false} closed={false} />);
     expect(document.querySelectorAll('.toggle-btn')[0].childNodes[0].nodeValue).toBe('Lock Gate');
    });
 
    it('check text on lock button if locked then Unlock Gate', () => {
-    const container = render(<Controls locked={true} closed={true} toggleLocked={toggleLocked} toggleClosed={toggleClosed}/>);
+    const container = render(<Controls locked={true} closed={true} />);
     expect(document.querySelectorAll('.toggle-btn')[0].childNodes[0].nodeValue).toBe('Unlock Gate');
    });
 
    it('check text on open button if closed then Open Gate', () => {
-    const container = render(<Controls locked={false} closed={true} toggleLocked={toggleLocked} toggleClosed={toggleClosed}/>);
+    const container = render(<Controls locked={false} closed={true} />);
     expect(document.querySelectorAll('.toggle-btn')[1].childNodes[0].nodeValue).toBe('Open Gate');
    });
 
    it('check text on open button if open then Close Gate', () => {
-    const container = render(<Controls locked={false} closed={false} toggleLocked={toggleLocked} toggleClosed={toggleClosed}/>);
+    const container = render(<Controls locked={false} closed={false} />);
     expect(document.querySelectorAll('.toggle-btn')[1].childNodes[0].nodeValue).toBe('Close Gate');
    });
 
    it('disable lock button if open', () => {
-    const container = render(<Controls locked={false} closed={false} toggleLocked={toggleLocked} toggleClosed={toggleClosed}/>);
+    const container = render(<Controls locked={false} closed={false} />);
     expect(document.querySelectorAll('.toggle-btn')[0]).toHaveAttribute('disabled');
    });
 
    it('disable open button if locked', () => {
-    const container = render(<Controls locked={true} closed={true} toggleLocked={toggleLocked} toggleClosed={toggleClosed}/>);
+    const container = render(<Controls locked={true} closed={true} />);
     expect(document.querySelectorAll('.toggle-btn')[1]).toHaveAttribute('disabled');
    });
+
+   ////////////////////////////////////////
+
+   it('check buttons call functions on click', () => {
+    const test = jest.fn();
+    const test2 = jest.fn();
+    const {container, getByText} = render(<Controls locked={false} closed={true} toggleLocked={test} toggleClosed={test2}/>);    
+    
+    const lockBtn = getByText('Lock Gate')
+    const openBtn = getByText('Open Gate')
+
+    fireEvent.click(lockBtn);
+    fireEvent.click(openBtn);
+    expect(test).toHaveBeenCalled();
+    expect(test2).toHaveBeenCalled();
+   });
+
+
 
 })
